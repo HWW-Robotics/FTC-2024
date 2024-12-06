e8ea052267170a569caff7708a72458d67723646
zyxkad <zyxkad@gmail.com>
2024-12-05 21:31:12 -0700

    automaticly record path in BPathRecorder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/BPathRecorder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/BPathRecorder.java
@@ -1,108 +1,182 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
-import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
+import android.content.Context;
 
-import org.firstinspires.ftc.teamcode.drive.ClawSlide;
-import org.firstinspires.ftc.teamcode.drive.MecanumDrive;
+import androidx.annotation.Nullable;
+
+import org.firstinspires.ftc.ftccommon.external.WebHandlerRegistrar;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.WebHandlerManager;
+
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
-@TeleOp(name = "BPathRecorder")
-public class BPathRecorder extends OpMode {
-    static final double MAX_DRIVE_POWER = 0.5;
+import java.util.ArrayList;
+import java.util.Formatter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-    MecanumDrive drive;
+import fi.iki.elonen.NanoHTTPD;
+
+@TeleOp(name = "BPathRecorder")
+public class BPathRecorder extends AMainTeleOp {
+    static final List<RecordNode> nodes = new ArrayList<>();
+
     SampleMecanumDrive driver;
-    ClawSlide clawSlide;
+
+    boolean last1X = false;
+    boolean last2A = false;
+    boolean last2B = false;
+    boolean last2X = false;
+    boolean last2Y = false;
 
     @Override
     public void init() {
-        this.drive = new MecanumDrive(
-                MAX_DRIVE_POWER,
-                hardwareMap.get(DcMotor.class, "rightFront"),
-                hardwareMap.get(DcMotor.class, "rightRear"),
-                hardwareMap.get(DcMotor.class, "leftFront"),
-                hardwareMap.get(DcMotor.class, "leftRear"));
+        super.init();
         this.driver = new SampleMecanumDrive(hardwareMap);
-        this.clawSlide = new ClawSlide(
-            hardwareMap.get(DcMotor.class, "leftRotation"),
-            hardwareMap.get(DcMotor.class, "rightRotation"),
-            hardwareMap.get(DcMotor.class, "leftSlide"),
-            hardwareMap.get(DcMotor.class, "rightSlide"),
-            hardwareMap.get(Servo.class, "ClawRotLeft"),
-            hardwareMap.get(Servo.class, "ClawRotRight"),
-            hardwareMap.get(Servo.class, "ClawArmLeft"),
-            hardwareMap.get(Servo.class, "ClawArmRight")
-        );
+        nodes.clear();
+        nodes.add(new PoseNode(this.driver.getLocalizer().getPoseEstimate()));
     }
 
     @Override
     public void loop() {
         com.acmerobotics.roadrunner.localization.Localizer localizer = this.driver.getLocalizer();
         localizer.update();
-
         Pose2d pos = localizer.getPoseEstimate();
 
-        boolean clawActioned = false;
-        if (this.gamepad2.dpad_up) {
-            this.clawSlide.slideRotate.move(-15);
-            clawActioned = true;
-        } else if (this.gamepad2.dpad_down) {
-            this.clawSlide.slideRotate.move(15);
-            clawActioned = true;
-        } else if (this.gamepad2.dpad_left) {
-            this.clawSlide.slideRotate.setPosition(0);
-            clawActioned = true;
-        }
-        if (this.gamepad2.left_stick_y != 0) {
-            this.clawSlide.slideLift.move((int) (-this.gamepad2.left_stick_y * 50));
-            clawActioned = true;
-        }
-        if (this.gamepad2.left_bumper) {
-            this.clawSlide.claw.closeLeft();
-        } else {
-            this.clawSlide.claw.openLeft();
-        }
-        if (this.gamepad2.right_bumper) {
-            this.clawSlide.claw.closeRight();
-        } else {
-            this.clawSlide.claw.openRight();
-        }
-        if (this.gamepad2.b) {
-            this.clawSlide.claw.setRotate(90);
-            clawActioned = true;
-        } else if (this.gamepad2.y) {
-            this.clawSlide.retractAndPullUp();
-        } else if (this.gamepad2.x) {
-            this.clawSlide.putDown();
-        } else if (this.gamepad2.a) {
-            this.clawSlide.putDownAndExtend();
-        } else if (this.gamepad2.right_stick_y != 0) {
-            this.clawSlide.claw.rotate(-this.gamepad2.right_stick_y * 5);
-            clawActioned = true;
-        }
-        if (clawActioned) {
-            this.clawSlide.cancelAction();
-        }
-        this.clawSlide.update();
-
-        this.drive.shift(this.gamepad1.left_stick_x, this.gamepad1.left_stick_y);
-        this.drive.rotate(this.gamepad1.right_stick_x * 0.5);
-        this.drive.updatePowers();
-
+        this.telemetry.addData("Nodes", nodes.size());
+        this.telemetry.addLine();
         this.telemetry.addData("Pos", "%+03.02f, %+03.02f", pos.getX(), pos.getY());
         this.telemetry.addData("Heading", Math.toDegrees(pos.getHeading()));
 
-        this.telemetry.addData("LeftSlideRot", this.clawSlide.slideRotate.getLeftPosition());
-        this.telemetry.addData("RightSlideRot", this.clawSlide.slideRotate.getRightPosition());
-        this.telemetry.addData("SlideMaxPos", this.clawSlide.slideLift.getMaxPosition());
-        this.telemetry.addData("LeftPosition", this.clawSlide.slideLift.getLeftPosition());
-        this.telemetry.addData("RightPosition", this.clawSlide.slideLift.getRightPosition());
-        this.telemetry.addData("LeftClawRot", this.clawSlide.claw.getLeftRotAngle());
-        this.telemetry.addData("RightClawRot", this.clawSlide.claw.getRightRotAngle());
-        this.telemetry.update();
+        super.loop();
+
+        boolean updatedPose = !this.last1X && this.gamepad1.x;
+        if (updatedPose) {
+            nodes.add(new PoseNode(pos));
+        }
+        this.last1X = this.gamepad1.x;
+
+        if (!updatedPose) {
+            PoseNode lastNode = getLastPoseNode();
+            Pose2d last2Pos = getSecondLastPose();
+            if (lastNode == null) {
+                if (Math.abs(last2Pos.getX() - pos.getX()) + Math.abs(last2Pos.getY() - pos.getY()) > 1 || Math.abs(last2Pos.getHeading() - pos.getHeading() + Math.PI * 2) % (Math.PI * 2) > 0.01) {
+                    nodes.add(new PoseNode(pos));
+                }
+            } else {
+                lastNode.pos = pos;
+            }
+        }
+        if (!this.last2A && this.gamepad2.a) {
+            nodes.add(new ActionNode("A"));
+        }
+        if (!this.last2B && this.gamepad2.b) {
+            nodes.add(new ActionNode("B"));
+        }
+        if (!this.last2X && this.gamepad2.x) {
+            nodes.add(new ActionNode("X"));
+        }
+        if (!this.last2Y && this.gamepad2.y) {
+            nodes.add(new ActionNode("Y"));
+        }
+        this.last2A = this.gamepad2.a;
+        this.last2B = this.gamepad2.b;
+        this.last2X = this.gamepad2.x;
+        this.last2Y = this.gamepad2.y;
+    }
+
+    @Nullable
+    static PoseNode getLastPoseNode() {
+        RecordNode n = nodes.get(nodes.size() - 1);
+        return n instanceof PoseNode ? (PoseNode) n : null;
+    }
+
+    static Pose2d getSecondLastPose() {
+        for (int i = nodes.size() - 2; i >= 0; i--) {
+            RecordNode n = nodes.get(i);
+            if (n instanceof PoseNode) {
+                return ((PoseNode) n).pos;
+            }
+        }
+        throw new IllegalStateException("No pose node found in the list!");
+    }
+
+    @WebHandlerRegistrar
+    public static void registerWebHandler(Context context, WebHandlerManager manager) {
+        manager.register("/recorded_paths", session -> {
+            StringBuilder builder = new StringBuilder();
+            Formatter fmt = new Formatter(builder);
+            builder.append("# Length: ").append(nodes.size());
+            Map<String, Object> data = new HashMap<>();
+            for (int i = 0; i < nodes.size(); i++) {
+                RecordNode n = nodes.get(i);
+                data.clear();
+                n.saveData(data);
+                fmt.format("%03d", i + 1);
+                builder
+                    .append("; ")
+                    .append(n.getType())
+                    .append(" { ");
+                boolean first = true;
+                for (Map.Entry<String, Object> kv : data.entrySet()) {
+                    if (first) {
+                        first = false;
+                    } else {
+                        builder.append(", ");
+                    }
+                    builder
+                        .append(kv.getKey())
+                        .append(": ")
+                        .append(kv.getValue());
+                }
+                builder.append(" }\n");
+            }
+            return NanoHTTPD.newFixedLengthResponse(NanoHTTPD.Response.Status.OK, NanoHTTPD.MIME_PLAINTEXT, builder.toString());
+        });
+    }
+}
+
+abstract class RecordNode {
+    abstract String getType();
+    abstract void saveData(Map<String, Object> data);
+}
+
+class PoseNode extends RecordNode {
+    Pose2d pos;
+    PoseNode(Pose2d pos) { this.pos = pos; }
+    @Override
+    String getType() { return "Pose2d"; }
+    @Override
+    void saveData(Map<String, Object> data) {
+        data.put("x", pos.getX());
+        data.put("y", pos.getY());
+        data.put("heading", Math.toDegrees(pos.getHeading()));
+    }
+}
+
+class RotorNode extends RecordNode {
+    final String rotor;
+    int pos;
+    RotorNode(String rotor, int pos) { this.rotor = rotor; this.pos = pos; }
+    @Override
+    String getType() { return "Rotor"; }
+    @Override
+    void saveData(Map<String, Object> data) {
+        data.put("rotor", rotor);
+        data.put("pos", pos);
+    }
+}
+
+class ActionNode extends RecordNode {
+    final String action;
+    ActionNode(String action) { this.action = action; }
+    @Override
+    String getType() { return "Action"; }
+    @Override
+    void saveData(Map<String, Object> data) {
+        data.put("act", action);
     }
 }
